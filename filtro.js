b3bd242b66f74cc190f1d38b30a7e877cb7639f7
Lorena Raygoza <lorenaraygoza0904@gmail.com>
2024-10-29 17:37:44 -0700

    feat: se añaden archivos semi corregidos, falta la funcionalidad del js

--- filtro.js
+++ filtro.js
@@ -1,63 +1,81 @@
 // Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  /* añadi un id a cada producto */
+  {id: 1, nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg", descripcion: "Tacón negro de charol"},
+  {id: 2, nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg", descripcion: "Zapatilla azul con moño y herraje plateado"},
+  {id: 3, nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg", descripcion: "Botín de gamusa negra con tacon"},
+  {id: 4, nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg", descripcion: "Bota de gamusa azul con tacón, cadena y herrajes dorados"},
+  {id: 5, nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg", descripcion: "Zapatilla de satín rojo con tacón pequeño"}
 ]
+/* cambie getElementsByName por getElementById,  añadi ";" al final y cambie li por list*/
+const list = document.getElementById("lista-de-productos");
+/* $i complete a $input */
+const $input = document.querySelector('.input');
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
-
+//cree la funcion displayProductos que se menciona mas abajo
+function displayProductos (productos) {
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  //cambie todas las var por let y cambie el nombre de las variables 
+  let card = document.createElement("div");
+  card.classList.add("producto");
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  let titulo = document.createElement("h3");
+  titulo.classList.add("titulo");
+  titulo.textContent = productos[i].nombre;
+
+  let subtitulo = document.createElement("p")
+  subtitulo.classList.add("subtitulo")
+  subtitulo.textContent = productos[i].descripcion;
   
-  var imagen = document.createElement("img");
+  let imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
+//añadi ;
+  card.appendChild(titulo);
+  card.appendChild(imagen);
+  card.appendChild(subtitulo);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
+  list.appendChild(card);
 }
-
-displayProductos(productos)
+}
+displayProductos(productos);
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
   }
 
-  const texto = $i.value;
-  console.log(texto);
+  const texto = $input.value.toLowerCase(); //converti el texto ingresado a minusculas
   const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+//funcion que muestra los productos filtrados
+  displayProductos(productosFiltrados);
+}
+const filtrados = (productos = [], texto) => {
+  return productos.filter(item=>
+    item.tipo.toLowerCase().includes(texto) ||
+    item.color.toLowerCase().includes(texto)
+  );
+}
+  /* for (let i = 0; i < productosFiltrados.length; i++) {
+    let card = document.createElement("div");
+    card.classList.add("producto");
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+    let titulo = document.createElement("h3");
+    titulo.classList.add("titulo");
+    titulo.textContent = productosFiltrados[i].nombre;
     
-    var imagen = document.createElement("img");
+    let imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    card.appendChild(titulo);
+    card.appendChild(imagen);
+    card.appendChild(subtitulo);
   
-    li.appendChild(d)
-  }
-}
+    list.appendChild(card);
+  } */
 
-const filtrado = (productos = [], texto) => {
+
+/* const filtrado = (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+}   */
